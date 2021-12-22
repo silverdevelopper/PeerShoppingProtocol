058b792887ea962708371581e439e523c6259f59
nurullah gumus <ngumus@aefis.com>
2021-12-22 10:27:00 +0300

    Addd products to database

--- src/models/product.py
+++ src/models/product.py
@@ -4,6 +4,13 @@ from models.base_model  import BaseModel
 from base_model import BaseModel
 
 class Product(BaseModel):
-    def __init__(self,name:str = "",desc:str = "", unit_key = "") -> None:
+    def __init__(
+        self,
+        name:str = "",
+        desc:str = "", 
+        unit_key = "",
+        amount = 0) -> None:
+        
         super().__init__(name,desc)
-        self.unit_key = unit_key
+        self.unit_key = unit_key,
+        self.amount:int = amount
